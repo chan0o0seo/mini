c5a7d4c4c92c1625640111e91b450e28d4895efa
scy <tjcksdud0221@naver.com>
2025-03-12 11:36:51 +0900

    Dockerfile

--- src/main/java/com/example/myapp/AppController.java
+++ src/main/java/com/example/myapp/AppController.java
@@ -23,5 +23,9 @@ public class AppController {
     public String test3() {
         return "test3";
     }
+    @GetMapping("/test4")
+    public String test4() {
+        return "test3";
+    }
 
 }
